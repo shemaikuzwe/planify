74ff610bbebee4abbc0cde0b6eb06af5ce795512
shemaikuzwe <eshemaikuzwe@gmail.com>
2025-04-18 23:30:05 +0200

    feat:setting db

--- app/auth.ts
+++ app/auth.ts
@@ -1,6 +1,9 @@
+import db from "@/lib/drizzle";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(db),
   providers: [Google],
 });
