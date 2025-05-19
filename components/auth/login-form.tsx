760f3aa62dd438fd486740838e26ca5e1ca385d4
shemaikuzwe <eshemaikuzwe@gmail.com>
2025-05-19 10:11:11 +0200

    fix login

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -5,20 +5,20 @@ import Logo from "../logo"
 import { signIn } from "next-auth/react"
 import GitHub from "./Github"
 import Google from "./Google"
+import TypingAnimation from "../ui/typing-animation"
 
 
 export default function LoginForm() {
   return (
     <div className="grid lg:grid-cols-2 min-h-screen w-full">
       <div className="hidden lg:flex flex-col justify-between bg-primary  w-full not-only:p-8 h-full">
-        <Logo className="bg-white"/>
-        <div className="my-8">
-      
+        <Logo className="bg-white" />
+        <div className="my-8 flex justify-start">
+          <TypingAnimation words={["Plan", "Create"]} />
         </div>
 
         <div className="space-y-4">
-          <h2 className="text-3xl font-bold">Welcome back to Planify</h2>
-          <p>Everything you need in an easily customizable dashboard.</p>
+          <p >Everything you need to plan and create.</p>
         </div>
 
         <div className="mt-8 text-sm text-muted-foreground">Copyright © {new Date().getFullYear()} Planify</div>
@@ -29,16 +29,16 @@ export default function LoginForm() {
           <Logo className="bg-gray-900" textClassName="text-gray-900" />
         </div>
 
-        <div className="w-full max-w-md border dark:bg-white rounded-xl border-gray-200 p-4 text-gray-900">
+        <div className="w-full max-w-md border dark:bg-white rounded-xl border-gray-200 p-4 text-gray-900 mb-5">
           <div className="space-y-2 text-center pb-4">
             <h2 className="text-2xl font-bold">Welcome back</h2>
             <p className="text-muted-foreground">
-              Sign in to your account using your preferred method
+              Sign in to your account 
             </p>
           </div>
           <div className="space-y-4 pt-0">
-            <form className="w-full" action={ () => {
-               signIn("github")
+            <form className="w-full" action={() => {
+              signIn("github")
             }}>
               <Button
                 className="w-full h-12  bg-slate-300  text-gray-900 hover:bg-slate-300 hover:opacity-90 flex gap-2 items-center justify-center mb-4"
@@ -49,8 +49,8 @@ export default function LoginForm() {
               </Button>
             </form>
 
-            <form className="w-full" action={ () => {
-              signIn("google",{redirectTo:"/"})
+            <form className="w-full" action={() => {
+              signIn("google", { redirectTo: "/" })
             }}>
               <Button
                 className="w-full h-12 text-base  bg-slate-300 text-gray-900 hover:bg-slate-300  hover:opacity-90 flex gap-2 items-center justify-center"
