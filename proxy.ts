0b6ff238eabed26e8f5e4df38726a61c130acf39
shemaikuzwe <eshemaikuzwe@gmail.com>
2025-10-16 17:48:14 +0200

    fix build

--- proxy.ts
+++ proxy.ts
@@ -17,8 +17,9 @@ export default auth((req) => {
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
-}
+  runtime: "nodejs",
+};
